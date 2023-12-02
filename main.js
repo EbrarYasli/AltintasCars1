e562bef6016dda8f0cface57cbe3f3304654d558
EbrarYasliPXL <12201323@student.pxl.be>
2023-12-02 09:03:45 +0100

    afgewerkt zonder inhoud

--- main.js
+++ main.js
@@ -12,3 +12,14 @@ let header = document.querySelector('header');
 window.addEventListener('scroll' , () =>{
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+//navigation bar
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');
+}
+//hide menu and search box when scrolling
+window.onscroll = () =>{
+    menu.classList.remove('active');
+}

5aa414ed6ae659f3cbcf4dace44b6a16edd4dfb5
Ebrar Yasli <ebrar_yasli2832@hotmail.com>
2025-05-26 22:22:09 +0200

    menu close fixed

--- main.js
+++ main.js
@@ -203,7 +203,7 @@ function openmenu() {
     backdrop.style.cssText = `
         position: fixed;
         top: 0;
-        left: 0;
+        right: 0;
         width: 100%;
         height: 100%;
         background: rgba(0, 0, 0, 0.5);
